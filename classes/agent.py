ab081dd255fa4e9b1a0edd3033860f7c2b5daf50
Frantisek Bartos <f.bartos96@gmail.com>
2021-01-15 12:49:01 +0100

    some updates
    - reducing multiplicity of computations (spawning of new fisherman, calculating of average valet / schoold size) by creating separate methods for them
    - extracting some hardcoded parameters into the model call

--- classes/agent.py
+++ classes/agent.py
@@ -33,37 +33,31 @@ class Fish(Random):
         curr_time = self.model.schedule_Fish.time
         self.move()
 
-        # New fish spawn yearly (added + 1 just so they don't reproduce immediately)
-        if (curr_time + 1) % 365 == 0:
+        # New fish spawn every 4 weeks (added + 1 just so they don't reproduce immediately)
+        if (curr_time + 1) % 4 == 0:
             self.size *= self.model.fish_reproduction_number
-            print('SIZE INCREASED TO', self.size)
+            # print('SIZE INCREASED TO', self.size)
 
         # Schools above N tonnes will split in half
         if self.size > self.model.split_size:
-            print('SPLITTING')
+            # print('SPLITTING')
             self.model.new_agent(Fish, self.pos, self.size*0.5, 0, True, 0)
             self.size *= 0.5
 
         # Schools under a threshold are removed
-        if self.size < 1:
+        if self.size < (self.model.initial_school_size / 20):
             self.model.remove_agent(self)
-        
-        total_fish = 0
-        for agent in self.model.agents:
-            total_fish += agent.size
-
-        average_school = total_fish / self.model.schedule_Fish.get_agent_count()
-        self.model.avg_school_size = average_school
-
 
 class Fisherman(Random):
     def __init__(self, id, model, pos, size, wallet, switch, init_wait_time):
         super().__init__(id, model, pos, size, wallet, switch, init_wait_time)
 
+        self.start_wait_time = 0
+        
     def step(self):
 
-        if self.switch == False:
-            curr_time = self.model.schedule_Fisherman.time
+        # fishing or waiting behavior
+        if not self.switch:
             self.move()
 
             surrounding = MultiGrid.get_neighbors(self.model.grid, self.pos, True, True,0)
@@ -81,35 +75,21 @@ class Fisherman(Random):
                         agent.size -= self.model.catch_rate
                         self.size += self.model.catch_rate
                 break
-
-            if self.size == self.model.max_load:
-                self.size = 0
-                self.wallet += self.model.initial_wallet
-                self.switch = True
-                self.init_wait_time = curr_time
             
-        # Pauzes if load is full
-        elif self.model.schedule_Fisherman.time - self.init_wait_time > 4:
+            if self.size == self.model.max_load:
+                self.size    = 0
+                self.wallet += self.model.full_catch_reward
+                self.switch  = True
+                self.start_wait_time = self.model.schedule_Fisherman.time
+            
+        # Pauses if load is full
+        elif self.start_wait_time + self.model.fisherman_wait_time > self.model.schedule_Fisherman.time:
             self.switch = False
 
-        total_wallet = 0
-        for agent in self.model.agents:
-            total_wallet += agent.wallet
+        # paying the weekly cost of living
+        self.wallet -= self.model.initial_wallet / self.model.initial_wallet_survival
 
-        wallet_mean = total_wallet / self.model.schedule_Fisherman.get_agent_count()
-        self.model.avg_wallet = wallet_mean
-
-        if wallet_mean > 40000:
-            for _ in range(max(5,int(self.model.schedule_Fisherman.get_agent_count()*0.05))):
-                self.model.new_agent(Fisherman, self.pos, 0, self.model.initial_wallet, False, 0)
-
-        # Daily cost of living
-        self.wallet -= 750
-
+        # removing ig going bankrupt
         if self.wallet <= 0:
-            if self.model.schedule_Fisherman.get_agent_count() <= 1:
-                self.model.new_agent(Fisherman, self.pos, 0, self.model.initial_wallet, False, 0)
             self.model.remove_agent(self)
         
-        
-
--- classes/model.py
+++ classes/model.py
@@ -13,10 +13,11 @@ class FishingModel(Model):
     '''
 
     def __init__(self, height=40, width=40,
-                 initial_fish=200, initial_fishermen=50,
-                 fish_reproduction_number=1.5, catch_rate=8,
-                 max_load=24, initial_wallet = 10000, initial_school_size = 100, split_size = 150):
-
+                 initial_fish=100, initial_fishermen=10,
+                 initial_school_size = 100, split_size = 200, fish_reproduction_number=1.03,
+                 initial_wallet = 100, catch_rate=15, max_load=30, full_catch_reward = 100, fisherman_wait_time = 2,
+                 initial_wallet_survival = 4*12*2, prop_plus_wallet_spawn = 20, avg_wallet_spawn_threshold = 1.05
+                 ):
         super().__init__()
 
         self.height = height
@@ -31,17 +32,25 @@ class FishingModel(Model):
         self.max_load = max_load
         self.avg_wallet = initial_wallet
         self.avg_school_size = initial_school_size
-
+        self.full_catch_reward = full_catch_reward
+        self.initial_wallet_survival = initial_wallet_survival
+        self.fisherman_wait_time = fisherman_wait_time
+        self.prop_plus_wallet_spawn = prop_plus_wallet_spawn
+        self.avg_wallet_spawn_threshold = avg_wallet_spawn_threshold
+        
         # Add a schedule for fish and fishermen seperately to prevent race-conditions
         self.schedule_Fish = RandomActivation(self)
         self.schedule_Fisherman = RandomActivation(self)
 
+        self.this_avg_wallet        = initial_wallet
+        self.this_avg_school_size   = initial_school_size
+        
         self.grid = MultiGrid(self.width, self.height, torus=True)
         self.datacollector = DataCollector(
              {"Fish": lambda m: self.schedule_Fish.get_agent_count(),
               "Fishermen": lambda m: self.schedule_Fisherman.get_agent_count(),
-              "Average wallet": lambda m: self.avg_wallet*0.01,
-              "Average school size": lambda m: self.avg_school_size})
+              "Average wallet": lambda m: self.this_avg_wallet,
+              "Average school size": lambda m: self.this_avg_school_size})
 
         # Keep a list of all agents
         self.agents = []
@@ -87,13 +96,54 @@ class FishingModel(Model):
         self.agents.remove(agent)
         getattr(self, f'schedule_{type(agent).__name__}').remove(agent)
 
+    def recruit_fisherman(self):
+        '''
+        Method that spawns new fisherman based on the average gains of the existing fisherman.
+        '''
+        n_fisherman   = self.schedule_Fisherman.get_agent_count()
+        profitability = self.this_avg_wallet / self.initial_wallet
+        
+        # make sure that at least one fisherman exists, who wouldn't try a new bussiness in a new field?
+        if n_fisherman == 0:
+            self.init_population(Fisherman, 1)
+        # add new fisherman proportional to the profitability
+        elif profitability > self.avg_wallet_spawn_threshold:
+                n_new_fisherman = int( (profitability / self.avg_wallet_spawn_threshold)*self.prop_plus_wallet_spawn)
+                print("SPAWNING NEW FISHERMAN ", n_new_fisherman)
+                self.init_population(Fisherman, n_new_fisherman)
+
+    def get_fish_stats(self):
+        '''
+        Method that computes information about fishes.
+        '''
+        fish_size = [fish.size for fish in self.schedule_Fish.agents]
+        if len(fish_size) == 0:
+            self.this_avg_school_size = 0
+        else:
+            self.this_avg_school_size = sum(fish_size) / len(fish_size)
+        
+    def get_fisherman_stats(self):
+        '''
+        Method that computes information about fisherman.
+        '''
+        fisherman_wallet = [fisherman.wallet for fisherman in self.schedule_Fisherman.agents]
+        if len(fisherman_wallet) == 0:
+            self.this_avg_wallet = 0
+        else:
+            self.this_avg_wallet = sum(fisherman_wallet) / len(fisherman_wallet)
+        
     def step(self):
         '''
         Method that calls the step method for each of the sheep, and then for each of the wolves.
         '''
         self.schedule_Fish.step()
         self.schedule_Fisherman.step()
-
+        
+        self.get_fish_stats()
+        self.get_fisherman_stats()
+        
+        self.recruit_fisherman()
+          
         # Save the statistics
         self.datacollector.collect(self)
 
